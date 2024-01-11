6f4a6ac928522dbc66b07776c93ff21fac708790
rajanbit <rajan22091998@gmail.com>
2024-01-11 19:08:08 +0530

    added gc & nt dist features

--- src/SeqPlotter/nt.py
+++ src/SeqPlotter/nt.py
@@ -1,5 +1,6 @@
 from SeqPlotter.sequence import Sequence
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 class DNA(Sequence):
 
@@ -14,9 +15,10 @@ class DNA(Sequence):
 		for nt in temp:
 			if nt in nt_comp:
 				temp[nt] = nt_comp[nt]
+		new_temp = {}
 		for k, v in temp.items():
-			temp[k] = (v/sum(temp.values()))*100
-		return temp
+			new_temp[k] = (v/sum(temp.values()))*100
+		return new_temp
 	
 	# Barplot
 	@staticmethod
@@ -30,14 +32,42 @@ class DNA(Sequence):
 	def gc_percent(self):
 		comp = self.comp()
 		return f'{((comp["G"]+comp["C"])/sum(comp.values()))*100:.2f}'
-	# GC percentage plot
+
+	# GC percentage plot | BARPLOT
 	@staticmethod
 	def gc_plot(records):
 		seq_dict = {}
 		for record in records:
 			seq_dict[record.seqid] = float(record.gc_percent())
 		DNA.barplot(seq_dict, "Sequences", "GC Content (%)")
+	
+	# GC distribution plot | BOXPLOT
+	@staticmethod
+	def gc_distribution_plot(records):
+		distr_lis = []
+		for record in records:
+			distr_lis.append(float(record.gc_percent()))
+		plt.boxplot(distr_lis, patch_artist=True, medianprops=dict(color='red', linewidth=1.5))
+		plt.xlabel("GC")
+		plt.ylabel("Percentage (%)")
+		plt.show()
 
+	# Nucleotide distribution plot | BOXPLOT
+	@staticmethod
+	def nt_distribution_plot(records):
+		nt_dict = defaultdict(list)
+		for record in records:
+			comp = record.comp()
+			nt_dict["A"].append(((comp["A"])/sum(comp.values()))*100)
+			nt_dict["T"].append(((comp["T"])/sum(comp.values()))*100)
+			nt_dict["G"].append(((comp["G"])/sum(comp.values()))*100)
+			nt_dict["C"].append(((comp["C"])/sum(comp.values()))*100)
+		plt.boxplot(nt_dict.values(), patch_artist=True, medianprops=dict(color='red', linewidth=1.5))
+		plt.xticks([1, 2, 3, 4], nt_dict.keys())
+		plt.xlabel("GC")
+		plt.ylabel("Percentage (%)")
+		plt.show()
+	
 class RNA(Sequence):
 
 	# Constructor
@@ -53,28 +83,37 @@ class RNA(Sequence):
 		for nt in temp:
 			if nt in nt_comp:
 				temp[nt] = nt_comp[nt]
+		new_temp = {}
 		for k, v in temp.items():
-			temp[k] = (v/sum(temp.values()))*100
-		return temp
-
-	# Barplot
-	@staticmethod
-	def barplot(comp_dict:dict, x_lab="Nucleotides", y_lab="Percentage (%)"):
-		plt.bar(list(comp_dict.keys()), list(comp_dict.values()))
-		plt.xlabel(x_lab)
-		plt.ylabel(y_lab)
-		plt.show()
+			new_temp[k] = (v/sum(temp.values()))*100
+		return new_temp
 	
 	# GC percentage
 	def gc_percent(self):
-		comp = self.comp()
-		return f'{((comp["G"]+comp["C"])/sum(comp.values()))*100:.2f}'
+		return DNA.gc_percent(self)
 	
-	# GC percentage plot
+	# GC percentage plot | BARPLOT
 	@staticmethod
 	def gc_plot(records):
-		seq_dict = {}
-		for record in records:
-			seq_dict[record.seqid] = float(record.gc_percent())
-		RNA.barplot(seq_dict, "Sequences", "GC Content (%)")
+		DNA.gc_plot(records)
 
+	# GC distribution plot | BOXPLOT
+	@staticmethod
+	def gc_distribution_plot(records):
+		DNA.gc_distribution_plot(records)
+
+	# Nucleotide distribution plot | BOXPLOT
+	@staticmethod
+	def nt_distribution_plot(records):
+		nt_dict = defaultdict(list)
+		for record in records:
+			comp = record.comp()
+			nt_dict["A"].append(((comp["A"])/sum(comp.values()))*100)
+			nt_dict["U"].append(((comp["U"])/sum(comp.values()))*100)
+			nt_dict["G"].append(((comp["G"])/sum(comp.values()))*100)
+			nt_dict["C"].append(((comp["C"])/sum(comp.values()))*100)
+		plt.boxplot(nt_dict.values(), patch_artist=True, medianprops=dict(color='red', linewidth=1.5))
+		plt.xticks([1, 2, 3, 4], nt_dict.keys())
+		plt.xlabel("GC")
+		plt.ylabel("Percentage (%)")
+		plt.show()
